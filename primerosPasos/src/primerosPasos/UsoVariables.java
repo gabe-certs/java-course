435abfbef585d3769f9cffb46ae18bacc1c1833e
Gabe <gabe.certs@gmail.com>
2025-02-25 19:00:16 -0600

    initial commit

---
+++ primerosPasos/src/primerosPasos/UsoVariables.java
@@ -0,0 +1,21 @@
+package primerosPasos;
+
+public class UsoVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte edad;
+		edad = 17;
+		var edad2 = 18;
+		
+		//cambiando el valor de la variable edad2
+		
+		edad2 = 35;
+		
+		System.out.println(edad);
+		System.out.println(edad2);
+		
+		
+	}
+
+}
